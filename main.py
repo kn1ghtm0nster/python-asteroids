b343bd3948e90789dd343f35ab0c3decd3467fd9
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-04-29 15:25:30 -0400

    add rotation logic to player ship to move left and right

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main() -> None:
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
