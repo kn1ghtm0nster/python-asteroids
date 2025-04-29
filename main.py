e6fbc7e4b4ba0735949b78c4c7afb5293b69f8ac
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-04-29 15:01:48 -0400

    update main.py to use Player class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main() -> None:
@@ -11,12 +12,14 @@ def main() -> None:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
