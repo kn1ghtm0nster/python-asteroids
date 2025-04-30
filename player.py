3c3e49e2f47e1eeb4390305719ae26fb41dbeede
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-04-30 16:57:15 -0400

    add cooldown logic between each shot fired

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 
 class Player(CircleShape):
@@ -13,6 +13,7 @@ class Player(CircleShape):
     def __init__(self, x, y) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.delay_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -37,6 +38,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.delay_timer = max(self.delay_timer - dt, 0)
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -61,6 +63,9 @@ class Player(CircleShape):
         """
         Creates a `Shot` in the direction the player is facing
         """
+        if self.delay_timer > 0:
+            return
+        self.delay_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = forward * PLAYER_SHOOT_SPEED
